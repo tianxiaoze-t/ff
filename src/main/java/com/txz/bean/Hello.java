26c4eed7aff451d565d0d3af8152bb1d3b92834a
tian <tian@qq.com>
2020-03-25 10:26:37 +0800

    xx

---
+++ src/main/java/com/txz/bean/Hello.java
@@ -0,0 +1,10 @@
+package com.txz.bean;
+
+public class Hello {
+
+	//hello类
+	public void gg(){
+		System.out.println("111");
+	}
+	
+}
---
+++ src/main/java/com/txz/bean/Test.java
@@ -0,0 +1,13 @@
+package com.txz.bean;
+
+public class Test {
+
+	//测试类
+	public static void main(String[] args) {
+		Hello n=new Hello();
+		n.gg();
+		
+	}
+		
+	}
+
